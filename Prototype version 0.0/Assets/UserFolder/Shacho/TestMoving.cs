3733cf697ea82c24a568cd90176b1844de994883
pusanmodoki <blackdragon.chanp@gmail.com>
2020-03-05 20:35:54 +0900

    NavMesh追加など

---
+++ Prototype version 0.0/Assets/UserFolder/Shacho/TestMoving.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMoving : MonoBehaviour
+{
+	[SerializeField]
+	UnityEngine.AI.NavMeshAgent m_navMeshAgent = null;
+	[SerializeField]
+	GameObject m_gameObject = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		m_navMeshAgent.destination = m_gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
