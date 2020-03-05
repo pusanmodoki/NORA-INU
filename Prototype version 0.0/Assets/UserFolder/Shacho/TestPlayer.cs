705e3cf568b00161623270db2759607c87c0dd79
pusanmodoki <blackdragon.chanp@gmail.com>
2020-03-05 17:41:17 +0900

    proto scene

---
+++ Prototype version 0.0/Assets/UserFolder/Shacho/TestPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : MonoBehaviour
+{
+	[SerializeField]
+	Rigidbody m_rigidBody = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+	void FixedUpdate()
+	{
+		Vector3 input = Vector3.zero;
+
+		input.z = Input.GetAxis("Vertical");
+		input.x = Input.GetAxis("Horizontal");
+
+		m_rigidBody.AddForce(input);
+	}
+}
