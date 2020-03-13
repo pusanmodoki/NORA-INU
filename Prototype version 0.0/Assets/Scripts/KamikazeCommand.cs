8e48adef901025d05d47ed3fe3be08899da4c620
pusanmodoki <blackdragon.chanp@gmail.com>
2020-03-13 19:34:13 +0900

    KAMIKAZE COMMAND

---
+++ Prototype version 0.0/Assets/Scripts/KamikazeCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KamikazeCommand : MonoBehaviour
+{
+	public void InvokeCommand(Vector3 direction, float timeoutSeconds)
+	{
+
+	}
+}
