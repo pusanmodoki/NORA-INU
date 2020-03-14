0858bf3e40bfed60d42d17665585934445f3267c
sayo <nununnu.0s8a2y0o@gmail.com>
2020-03-15 01:31:57 +0900

    カメラのズームアウト

---
+++ Prototype version 0.0/Assets/UserFolder/Sayonnu/Camera/CameraOperation.cs
@@ -0,0 +1,34 @@
+﻿//--------------------------------------------------------------
+// CameraOperation.cs
+// カメラ操作
+// 作成者：綾野紗世
+//--------------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOperation : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("追従するオブジェクト")]
+    private GameObject followingObject;
+
+    [SerializeField]
+    [Tooltip("カメラを動かすスピード")]
+    private float leaveSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // ズームイン・ズームアウト
+        this.transform.position =
+        Vector3.Lerp(this.transform.position, followingObject.transform.position, leaveSpeed);
+    }
+} 
---
+++ Prototype version 0.0/Assets/UserFolder/Sayonnu/Camera/CameraSwitching.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitching : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("ズームイン最小値")]
+    private float minZoom;
+
+    [SerializeField]
+    [Tooltip("ズームアウト最大値")]
+    private float maxZoom;
+
+    private int m_zoomMode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            this.transform.localPosition = new Vector3(0.0f, maxZoom, -10.0f);
+        }
+
+    }
+}
