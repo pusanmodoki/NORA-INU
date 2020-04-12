bdb5958e96a9e59d2b0c15253f00efc7815db4aa
ryutaro matsumoto <ryutaro.matsumoto0219@gmail.com>
2020-04-13 07:48:06 +0900

    [add]各シーンを追加 シーン遷移用のボタンを実装 簡易的なタイトル、ステージ選択画面を作成

---
+++ OneMark/Assets/Scripts/Menu/SceneTransButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneTransButton : MonoBehaviour
+{
+    [SerializeField]
+    string sceneName = "";
+    public void OnButtonSceneTrans()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
