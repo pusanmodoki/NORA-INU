f0024e2d95a950cf597db28e0af2ccde380db471
pusanmodoki <blackdragon.chanp@gmail.com>
2020-04-18 00:24:08 +0900

    Manager修正, Animation失敗するもエラーはなし

--- OneMark/Assets/Scripts/Dogs/DogAnimationController.cs
+++ OneMark/Assets/Scripts/Dogs/DogAnimationController.cs
@@ -10,7 +10,7 @@ public class DogAnimationController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+		m_animator.SetInteger("State", 1);  
     }
 
     // Update is called once per frame
