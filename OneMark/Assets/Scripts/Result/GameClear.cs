8852f24a292b58275d37c474efb4d665c57fc497
sayo <nununnu.0s8a2y0o@gmail.com>
2020-04-24 21:50:37 +0900

    メス犬アニメーションとUIいろいろ

---
+++ OneMark/Assets/Scripts/Result/GameClear.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameClear : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerInput clear;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clear.GameClearAnimation();
+        ResultCall.GameClear();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
