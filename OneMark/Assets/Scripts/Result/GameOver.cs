ac417bf02b2cc044e2e3c81e5b3278c97c9f6ff5
sayo <nununnu.0s8a2y0o@gmail.com>
2020-05-03 23:23:28 +0900

    ゲームオーバーのアニメーションとUI演出

---
+++ OneMark/Assets/Scripts/Result/GameOver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerInput overAnime;
+
+    [SerializeField]
+    private FollowObject result;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        overAnime.GameOverAnimation();
+        ResultCall.GameOver();
+        result.ResultFlg();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
