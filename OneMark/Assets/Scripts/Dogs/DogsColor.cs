b4c3398cfa4e47af558dd99aa201677bfc50eae6
ryutaro matsumoto <ryutaro.matsumoto0219@gmail.com>
2020-05-05 22:34:28 +0900

    [update]とりあえず

---
+++ OneMark/Assets/Scripts/Dogs/DogsColor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogsColor : MonoBehaviour
+{
+    [SerializeField]
+    GameObject body = null;
+
+    [SerializeField]
+    DogAIAgent dogInfo = null;
+    private void Update()
+    {
+        dogInfo = GetComponent<DogAIAgent>();
+        Material mat = body.GetComponent<SkinnedMeshRenderer>().materials[0];
+
+        switch (dogInfo.aiAgentInstanceID)
+        {
+            case 0:
+                {
+                    mat.color = Color.blue;
+                    break;
+                }
+            case 1:
+                {
+                    mat.color = Color.red;
+                    break;
+                }
+            case 2:
+                {
+                    mat.color = Color.green;
+                    break;
+                }
+        }
+    }
+
+}
