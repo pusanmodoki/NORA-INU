2bce1d0cc8fb525f59cd9bbfe32776fbcdf9b37f
ryutaro matsumoto <ryutaro.matsumoto0219@gmail.com>
2020-05-21 18:03:24 +0900

    [add]実装済みのステージにモグラ適応

---
+++ OneMark/Assets/Scripts/Animation/AnimationPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationPlayer : MonoBehaviour
+{
+    [SerializeField, Tooltip("操作したいアニメーションコントローラ")]
+    private Animator m_animator = null;
+
+
+    public void SetTrigger(string _parameterName)
+    {
+        m_animator.SetTrigger(_parameterName);
+    }
+
+    public void SetState(int _integer, string _parameterName)
+    {
+        m_animator.SetInteger(_parameterName, _integer);
+    }
+
+    public void SetFloat(float _float, string _parameterName)
+    {
+        m_animator.SetFloat(_parameterName, _float);
+    }
+
+    public void SetBool(bool _bool, string _parameterName)
+    {
+        m_animator.SetBool(_parameterName, _bool);
+    }
+}
