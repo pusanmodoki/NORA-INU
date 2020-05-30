4186872175d49178908e4208a7d5a70c32193d5c
ryutaro matsumoto <ryutaro.matsumoto0219@gmail.com>
2020-05-31 00:47:17 +0900

    [add]Fukidashiエフェクトプレハブ作成

---
+++ OneMark/Assets/Scripts/UI/3DUI/BillBoard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillBoard : MonoBehaviour
+{
+    [SerializeField]
+    Transform m_camera = null;
+
+    private void Update()
+    {
+        transform.LookAt(m_camera.position);
+    }
+
+}
