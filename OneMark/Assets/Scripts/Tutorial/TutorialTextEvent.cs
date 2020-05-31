04c238d5b591bfb4cba3ad10a1a87609d868b656
ryutaro matsumoto <ryutaro.matsumoto0219@gmail.com>
2020-05-31 14:15:32 +0900

    [add]Tutorialを追加

---
+++ OneMark/Assets/Scripts/Tutorial/TutorialTextEvent.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTextEvent : MonoBehaviour
+{
+    Dictionary<string, GameObject> m_eventTexts = new Dictionary<string, GameObject>();
+
+    [SerializeField]
+    GameObject activeText = null;
+
+    private void Start()
+    {
+        for(int i = 0; i < transform.childCount; ++i)
+        {
+            m_eventTexts.Add(transform.GetChild(i).name, transform.GetChild(i).gameObject);
+        }
+    }
+
+
+    private void Update()
+    {
+        switch (OneMarkSceneManager.instance.nowStageSceneIndex.y)
+        {
+            case 1:
+                {
+                    Stage1Event();
+                    break;
+                }
+            case 2:
+                {
+                    Stage2Event();
+                    break;
+                }
+        }
+    }
+
+    void Stage1Event()
+    {
+        if(!m_eventTexts["text1-2"].activeSelf && PlayerAndTerritoryManager.instance.mainPlayer.allTerritorys.Count == 2)
+        {
+            activeText.SetActive(false);
+            m_eventTexts["text1-2"].SetActive(true);
+            activeText = m_eventTexts["text1-2"];
+        }
+        if (!m_eventTexts["text1-3"].activeSelf && MainGameManager.instance.m_resultState == MainGameManager.ResultState.GameClear)
+        {
+            activeText.SetActive(false);
+            m_eventTexts["text1-3"].SetActive(true);
+            activeText = m_eventTexts["text1-3"];
+        }
+    }
+
+
+
+    void Stage2Event()
+    {
+        if(m_eventTexts["text2-1"].activeSelf)
+        {
+            foreach(var servant in ServantManager.instance.servantByMainPlayer)
+            {
+                if (!servant.linkMarkPoint)
+                {
+                    return;
+                }
+            }
+            m_eventTexts["text2-1"].SetActive(false);
+            m_eventTexts["text2-2"].SetActive(true);
+        }
+        else if (m_eventTexts["text2-2"].activeSelf)
+        {
+            foreach (var servant in ServantManager.instance.servantByMainPlayer)
+            {
+                if (!servant.linkMarkPoint)
+                {
+                    m_eventTexts["text2-2"].SetActive(false);
+                    m_eventTexts["text2-3"].SetActive(true);
+                }
+            }
+        }
+        else if (m_eventTexts["text2-3"].activeSelf && 
+            (MainGameManager.instance.m_resultState == MainGameManager.ResultState.GameClear ||
+            MainGameManager.instance.m_resultState == MainGameManager.ResultState.GameEnd))
+        {
+            m_eventTexts["text2-3"].SetActive(false);
+            m_eventTexts["text2-4"].SetActive(true);
+        }
+    }
+    void Stage3Event()
+    {
+    }
+    void Stage4Event()
+    {
+    }
+}
---
+++ OneMark/Assets/Shader/OneMarkCharacterToonShader.shader
@@ -0,0 +1,86 @@
+﻿Shader "Custom/OneMarkCharacterToonShader"
+{
+	Properties{
+			_Color("Color", Color) = (1,1,1,1)
+			_MainTex("Albedo (RGB)", 2D) = "white" {}
+			_RampTex("Ramp", 2D) = "white"{}
+			_BumpMap("Bumpmap", 2D) = "bump" {}
+	}
+		SubShader{
+				Tags { "RenderType" = "Opaque" }
+				LOD 200
+				Cull Front
+
+				CGPROGRAM
+				#pragma surface surf ToonRamp
+				#pragma target 3.0
+
+				sampler2D _MainTex;
+				sampler2D _RampTex;
+				sampler2D _BumpMap;
+
+				struct Input {
+						float2 uv_MainTex;
+						float2 uv_BumpMap;
+				};
+
+				fixed4 _Color;
+
+				fixed4 LightingToonRamp(SurfaceOutput s, fixed3 lightDir, fixed atten)
+				{
+
+						half d = dot(s.Normal, lightDir)*0.5 + 0.5;
+						d *= atten;
+						fixed3 ramp = tex2D(_RampTex, fixed2(d, 0.5)).rgb;
+						fixed4 c;
+						c.rgb = s.Albedo * _LightColor0.rgb * ramp * 2;
+						c.a = s.Alpha;
+						return c;
+				}
+
+				void surf(Input IN, inout SurfaceOutput o) {
+						fixed4 c = tex2D(_MainTex, IN.uv_MainTex);
+						o.Albedo = c.rgb;
+						o.Alpha = c.a;
+						o.Normal = UnpackNormal(tex2D(_BumpMap, IN.uv_BumpMap));
+				}
+				ENDCG
+					Cull Back
+
+					CGPROGRAM
+#pragma surface surf ToonRamp
+#pragma target 3.0
+
+					sampler2D _MainTex;
+				sampler2D _RampTex;
+				sampler2D _BumpMap;
+
+				struct Input {
+					float2 uv_MainTex;
+					float2 uv_BumpMap;
+				};
+
+				fixed4 _Color;
+
+				fixed4 LightingToonRamp(SurfaceOutput s, fixed3 lightDir, fixed atten)
+				{
+
+					half d = dot(s.Normal, lightDir)*0.5 + 0.5;
+					d *= atten;
+					fixed3 ramp = tex2D(_RampTex, fixed2(d, 0.5)).rgb;
+					fixed4 c;
+					c.rgb = s.Albedo * _LightColor0.rgb * ramp * 2;
+					c.a = s.Alpha;
+					return c;
+				}
+
+				void surf(Input IN, inout SurfaceOutput o) {
+					fixed4 c = tex2D(_MainTex, IN.uv_MainTex);
+					o.Albedo = c.rgb;
+					o.Alpha = c.a;
+					o.Normal = UnpackNormal(tex2D(_BumpMap, IN.uv_BumpMap));
+				}
+				ENDCG
+	}
+		FallBack "Diffuse"
+}
