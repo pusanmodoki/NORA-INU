ca3eb8d2b6f5c655a6f4f5a475427ddc9cac2fac
matsumoto0219 <ryutaro.matsumoto0219@gmail.com>
2020-06-05 22:31:54 +0900

    [add]電柱、ジャンプパッドのテクスチャ修正 カメラ挙動ベースを作ろうとしたやつ追加

---
+++ OneMark/Assets/Scripts/Camera/MainCamera/MainCameraNavigation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraNavigation : MonoBehaviour
+{
+
+    [SerializeField]
+    Dictionary<string, NavigationInfo> m_navInfo = new Dictionary<string, NavigationInfo>();
+
+    [SerializeField]
+    NavigationInfo m_nowNavigation = null;
+
+    [SerializeField]
+    string m_nowNavigationName = "";
+    
+    Transform m_startPoint = null;
+
+    float m_t = 0.0f;
+    float m_distance = 0.0f;
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        
+    }
+
+    void NormalNavigation()
+    {
+       
+    }
+
+
+    void SelectNavigation(string _name)
+    {
+        if (m_navInfo[_name] == null) { return; }
+        m_nowNavigation = m_navInfo[_name];
+    }
+}
---
+++ OneMark/Assets/Scripts/Camera/MainCamera/NavigationInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationInfo : MonoBehaviour
+{
+    public Transform lookPoint { get; set; } = null;
+
+    public Transform movePoint { get; set; } = null;
+
+    public float moveSpeed { get; set; } = 0.0f;
+
+    public enum MoveMode
+    {
+        Normal = 0,
+        Delay
+    }
+
+    MoveMode mode { get; set; } = MoveMode.Normal;
+}
