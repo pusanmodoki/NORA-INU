05421695424234785f001bf0d9239521e68ab349
sayo <nununnu.0s8a2y0o@gmail.com>
2020-06-09 17:51:57 +0900

    チームロゴ

---
+++ OneMark/Assets/Scripts/UI/TitileLogo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitileLogo : MonoBehaviour
+{
+    public void ToTitle()
+    {
+        OneMarkSceneManager.instance.MoveScene(OneMarkSceneManager.SceneState.Title);
+    }
+}
